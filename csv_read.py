7bf6e3b725875cf2d67a6825b5f47b201547730f
22thomju <49483189+22thomju@users.noreply.github.com>
2019-12-19 09:14:03 -0500

    Add files via upload

---
+++ csv_read.py
@@ -0,0 +1,4 @@
+with open("movies.csv", newline="") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row[0] + " (" + str(row[1]) + ")")
---
+++ text_write.py
@@ -0,0 +1,2 @@
+with open("members.txt", "w") as file:
+    file.write("John Cleese\n")
